981958d430f3966b566f6fe1bba1c9f825dcc665
omkadri <omkadri@hotmail.com>
2020-01-11 17:33:53 -0500

    Initialized repository
    - So far, game contains LevelController, Loading Screen, and Start Menu

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] int timeToWait = 4;
+    int currentSceneIndex;
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+        }
+    }
+        IEnumerator WaitForTime()
+        {
+            yield return new WaitForSeconds(timeToWait);
+            LoadNextScene(); 
+        }
+
+        public void LoadNextScene()
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1); //because in build index, Loading screen is 0 and Start Screen is 1
+        }
+
+    }
+
+
