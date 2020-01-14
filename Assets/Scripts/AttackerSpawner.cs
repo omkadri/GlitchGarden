4fdf0331df60d2073d92140025c2e4e8dd2dfa12
omkadri <omkadri@hotmail.com>
2020-01-14 09:39:48 -0500

    11. Created enemy spawner using coroutine.
    - Created an AttackerSpawner game object with AttackerSpawner.cs attached to it.
    - Converted the Start() function in AttackerSpawner.cs into a coroutine (it yeilds randomly to a custom range measured in seconds)
    - AttackerSpawner.cs requires an Attacker type prefab to be attached to it. The SpawnAttacker() function instantiates the attached object to the position of the enemy spawner object.
    
    Houskeeping
    - Prefabed several game objects
    - Made grass square invisible

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] float minSpawnDelay = 1f;
+    [SerializeField] float maxSpawnDelay = 1f;
+    [SerializeField] Attacker attackerPrefab;
+
+    bool spawn = true;
+
+
+    IEnumerator Start()
+    {
+        while (spawn) // while spawn = true
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    void Update()
+    {
+        
+    }
+}
