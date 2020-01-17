7ac2523ccae446a32f240eca09993789f56387e9
omkadri <omkadri@hotmail.com>
2020-01-17 12:52:56 -0500

    15. Created Projectile.cs which moves Zucchini across sreen
    - in Projectile.cs, I used transform.translate to move the Zucchini across the screen.
    - I attached Projectile.cs to the Zucchini prefab
    - I used code over keyframe animation because I can easily adjust the speed variable, as well as apply the script to other projectile prefabs.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 1;
+    void Update()
+    {
+        transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
+    }
+}
