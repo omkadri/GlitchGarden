68fb0b110a8a7f79e95c72d5732c937ae0cecf0b
omkadri <omkadri@hotmail.com>
2020-01-17 12:20:54 -0500

    14. Zucchini projectile now spawns on cactus hand during attack animation
    - Created shooter.cs that is now attached to cactus game object
    - Within shooter.cs, created 2 serialized gameObject variables (projectile and gun)
    - Created Fire() function that is called through an animation event (cactus attack animation)
    - created gun sub-object within cactus game object for projectile to spawn on
    - prefabed and linked scripts accordingly

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile, gun;// Serialized fields of the same object type can be declared on the same line
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, transform.rotation); //notice how we are instantiating at the gun's transform position
+    }
+}
