49c44962c59a9534d9ac85e84b8df28a5a37eb78
omkadri <omkadri@hotmail.com>
2020-01-23 13:00:20 -0500

    18. Zucchini now damages and kills lizard
    - Created health.cs and attached to lizard prefab. (set if condtion to destroy lizard when health reaches zero)
    - health.cs gives lizard a health variable and a DamageDeal() function
    - modified projectile.cs to get health from colliding object (in this case the lizard)
    - projectile.cs then calls DamageDeal() from the lizard. Set value to 50 (same as lizards health so he can die in one hit)
    - Zucchini now kills lizard on impact

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,8 +5,22 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 1;
+    [SerializeField] float damageAmount = 50f;
     void Update()
     {
         transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
     }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider) // we are specifying that this is any "other" objcet that collides with the zucchini
+    {
+        Debug.Log("I hit: "+ otherCollider.name);
+
+        // Reduce health
+        var health = otherCollider.GetComponent<Health>(); //this line gets the health script attached to the object comm
+        health.DealDamage(damageAmount);
+    }
 }
+
+
+// null error protection
+//if (otherCollider.GetComponent<Health>() != null)
