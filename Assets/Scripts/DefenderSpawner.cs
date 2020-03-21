8728fda7f912d480c3983ba38b003d784ae6a1c8
omkadri <omkadri@hotmail.com>
2020-03-21 14:13:32 -0400

    21. Cactus defender now spawns when mouse is clicked
    - Created DefenderSpawner.cs - instantiates cactus prefab when mouse is clicked
    
    Housekeeping
    
    - Fixed cactus prefab so that all instances have a "gun" to fire projectiles from

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject DefenderType;
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse was clicked");
+        SpawnDefender();
+    }
+
+    private void SpawnDefender()
+    {
+        GameObject newDefender = Instantiate(DefenderType, transform.position, Quaternion.identity) as GameObject; //"as gameObject" allows us to see the instance in the unity inspector, as well as interact with it
+    }
+
+}
+
