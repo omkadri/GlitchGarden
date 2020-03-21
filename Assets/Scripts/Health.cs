a9d248cbc4999946313d2b91eebf901b3a48bca0
omkadri <omkadri@hotmail.com>
2020-03-21 13:18:06 -0400

    20. Implemented VFX particles for when lizard dies
    - downloaded vfx file from gamedev.tv
    
    - vfx object type can now be attached to to health.cs
    
    - created TriggerDeathVFX() function in health.cs
    
    Housekeeping:
    
    - Adjusted minSpawnDelay and MaxSpawnDelay for lizard
    
    - Adjusted Lizard spawner position so that lizard moves one a grid by grid basis
    
    - Increased Speed of Zucchini

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] float health = 100f;
+    [SerializeField] GameObject deathVFX;
 
     public void DealDamage(float damage)
     {
         health -= damage;
         if (health <= 0)
         {
+            TriggerDeathVFX();
             Destroy(gameObject);
         }
+    }
+
+    private void TriggerDeathVFX()
+    {
+        //protects us against null returns
+        if (!deathVFX) { return; }
+
+        //instantiates vfx particle, and then destroy it
+        GameObject deathVFXObject = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(deathVFXObject, 1f);
     }
 }
