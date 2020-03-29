64d1b2e74a21b6ef19a518fa9b2b5d8054807125
omkadri <omkadri@hotmail.com>
2020-03-29 00:10:27 -0400

    27. Added currency display system
    - The main currency  in the game is stars.
    
    - Updated Buttons prefab to include UI display of stars (with custom font)
    
    - Created StarDisplay.cs, which includes functions for updating UI, adding stars, and spending stars.

---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour
+{
+    [SerializeField] int stars = 100;
+    Text starText;
+    void Start()
+    {
+        starText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        starText.text = stars.ToString();
+    }
+
+    //earning stars
+    public void AddStars(int amount)
+    {
+        stars += amount;
+        UpdateDisplay();
+    }
+
+    //using stars
+    public void SpendStars(int amount)
+    {
+        if (stars >= amount)//stops us from spending more stars then we have
+        {
+            stars -= amount;
+            UpdateDisplay();
+        }
+        
+    }
+}
